387a8e20bab8ce2713956b3e92f542ead8864ee1
Kartikesh-19 <kartikeshsharma59@gmail.com>
2024-01-08 12:00:11 +0530

    upd

--- src/app/api/hello/route.tsx
+++ src/app/api/hello/route.tsx
@@ -1,7 +1,6 @@
 // src/app/api/hello.ts
-import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function GET(req: any, res: any) {
   console.log("first", req);
   res.status(200).json({ message: 'Hello, Next.js!' });
 }
