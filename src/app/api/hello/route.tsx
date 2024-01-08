68552c7907076656d7e28e62a28b10f87bc357e8
Kartikesh-19 <kartikeshsharma59@gmail.com>
2024-01-08 11:17:49 +0530

    upd

--- src/app/api/hello/route.tsx
+++ src/app/api/hello/route.tsx
@@ -1,3 +1,4 @@
-// export async function GET(request:any) {
-//     return new Response('Hello, Next.js!')
-//   }
+export async function GET(request:any) {
+  console.log("first",request)
+    return new Response('Hello, Next.js!')
+  }
